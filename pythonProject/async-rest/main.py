13d74cdbd2422dd7d4538e5d3fbdd09f7f2bda1e
Gison Win <wenjx@linose.com>
2024-05-09 18:01:00 +0800

    async invoke database

--- pythonProject/async-rest/main.py
+++ pythonProject/async-rest/main.py
@@ -3,5 +3,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# 本项目参考地址 https://www.cnblogs.com/weiweivip666/p/18041425
+
 if __name__ == '__main__':
     uvicorn.run(app='main:app', host='0.0.0.0', port=8801, reload=True)
