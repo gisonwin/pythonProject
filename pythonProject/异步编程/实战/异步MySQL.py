689603980463db71bcb13d357bbdbc9e6c204415
Gison Win <wenjx@linose.com>
2024-06-11 16:54:13 +0800

    async invoke redis and mysql

--- pythonProject/异步编程/实战/异步MySQL.py
+++ pythonProject/异步编程/实战/异步MySQL.py
@@ -5,8 +5,8 @@ import asyncio, aiomysql
 async def execute(host, password, db):
     conn = await aiomysql.connect(host=host, port=3306, user='root', password=password, db=db)
     cur = await conn.cursor()
-    await cur.execute('select * from user')
-    result = await  cur.fetchall()
+    await cur.execute("select * from teacher")
+    result = await cur.fetchall()
     print(result)
     await cur.close()
     conn.close()
@@ -14,7 +14,7 @@ async def execute(host, password, db):
 
 task_list = [
     # execute(host="127.0.0.1", password="root", db='fastapi'),
-    execute(host="127.0.0.1", password="root", db='hq')
+    execute(host="127.0.0.1", password="root", db='fastapi')
 ]
 
 asyncio.run(asyncio.wait(task_list))
