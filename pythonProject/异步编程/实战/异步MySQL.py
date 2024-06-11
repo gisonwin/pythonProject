cc8862f1d3524e88cc525be3ad00b8aa24d7887e
Gison Win <wenjx@linose.com>
2024-06-11 16:44:53 +0800

    async invoke redis and mysql

---
+++ pythonProject/异步编程/实战/异步MySQL.py
@@ -0,0 +1,20 @@
+## pip install aiomysql
+import asyncio, aiomysql
+
+
+async def execute(host, password, db):
+    conn = await aiomysql.connect(host=host, port=3306, user='root', password=password, db=db)
+    cur = await conn.cursor()
+    await cur.execute('select * from user')
+    result = await  cur.fetchall()
+    print(result)
+    await cur.close()
+    conn.close()
+
+
+task_list = [
+    # execute(host="127.0.0.1", password="root", db='fastapi'),
+    execute(host="127.0.0.1", password="root", db='hq')
+]
+
+asyncio.run(asyncio.wait(task_list))
--- pythonProject/异步编程/实战/异步操作redis.py
+++ pythonProject/异步编程/实战/异步操作redis.py
@@ -4,23 +4,53 @@ import asyncio
 import aioredis
 
 
-async def execute():
-    redis = await aioredis.from_url("redis://localhost", decode_responses=True)
+def get_redis(address, password, max_connections=10, decode_responses=True):
+    # return aioredis.from_url('redis://124.70.89.186:26502', password="redis@sailmi", decode_responses=True,
+    #                          max_connections=10)
+    return aioredis.from_url(address, password=password, decode_responses=decode_responses,
+                             max_connections=max_connections)
 
-    await redis.hset("hash", mapping={"key1": "value1", "key2": "value2", "key3": 123})
 
+async def redis_hset():
+    redis = get_redis('redis://124.70.89.186:26502', decode_responses=True, password="redis@sailmi")
+    num = await redis.hset("hash", mapping={"name": "value1", "nick": "value2", "age": 123})
+    print(num)
     result = await redis.hgetall("hash")
+    print(result)
     assert result == {
-        "key1": "value1",
-        "key2": "value2",
-        "key3": "123",  # note that Redis returns int as string
+        "name": "value1",
+        "nick": "value2",
+        "age": "123",  # note that Redis returns int as string
     }
-    # await redis.set('key', 'value')
-    # value = await redis.get('key')
-    # print(value)
-
     await redis.close()
 
 
+async def execute_get_set():
+    redis = aioredis.from_url('redis://124.70.89.186:26502', password="redis@sailmi")
+    await redis.set("key", "string-value")
+    value = await redis.get("key")
+    print(value)
+    assert value == b'string-value'
+    redis = get_redis('redis://124.70.89.186:26502', decode_responses=True, password="redis@sailmi")
+    str_value = await redis.get("key")
+    assert str_value == "string-value"
+    await redis.close()
+
+
+async def redis_tran():
+    redis = get_redis('redis://124.70.89.186:26502', decode_responses=True, password="redis@sailmi")
+    async with redis.pipeline(transaction=True) as pipe:
+        ok1, ok2 = await (pipe.set("key1", "value1").set("key2", "value2").execute())
+        assert ok1
+        assert ok2
+        vl = pipe.mget("key1", "key2")
+        print(type(vl))
+
+
 if __name__ == '__main__':
-    asyncio.run(execute())
+    # 执行get set
+    asyncio.run(execute_get_set())
+    # 执行redis hset
+    asyncio.run(redis_hset())
+    # 执行transaction
+    asyncio.run(redis_tran())
